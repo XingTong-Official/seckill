52666c5c4f2edd959cbfb0c3b156fa5f9c79e3ef
XingTong-Official <2896309070@qq.com>
2025-04-13 20:06:26 +0800

    0.33 完成了service类的全部内容，添加了spring文件配置，下一步进行测试

--- src/main/java/com/clxxx/service/SeckillServiceImpl.java
+++ src/main/java/com/clxxx/service/SeckillServiceImpl.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
 
 import java.util.Date;
@@ -39,6 +40,7 @@ public class SeckillServiceImpl implements SeckillService{
     }
 
     @Override
+    @Transactional
     public Exposer exportSeckillUrl(Long id) {
         SecKill secKill = seckillDao.queryById(id);
         if(secKill == null||secKill.getNumber()<=0){
